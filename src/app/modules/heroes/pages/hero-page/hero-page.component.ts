0d06d0cb55cc5d772287746a8beb3e6b62de3e67
Hanns Espinoza <hanssabinadie@gmai.com>
2023-05-16 11:37:51 -0600

    feat: :sparkles: add search text

--- src/app/modules/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/modules/heroes/pages/hero-page/hero-page.component.ts
@@ -3,13 +3,13 @@ import { Router } from '@angular/router';
 import { Hero } from '@modules/heroes/models';
 
 import { HeroesService } from '@modules/heroes/services';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-hero-page',
-  templateUrl: './hero-page.component.html'
+  templateUrl: './hero-page.component.html',
 })
-export class HeroPageComponent implements OnInit{
-
+export class HeroPageComponent implements OnInit {
   @Input() id!: string;
 
   public hero?: Hero;
@@ -22,12 +22,19 @@ export class HeroPageComponent implements OnInit{
   }
 
   getHero(): void {
-    this.heroesService.getHeroById(this.id).subscribe((hero) => {
-      if (!hero) return this.router.navigate(['/heroes']);
+    this.heroesService
+      .getHeroById(this.id)
+      .pipe(delay(1000))
+      .subscribe((hero) => {
+        if (!hero) return this.router.navigate(['/heroes']);
 
-      this.hero = hero;
-      console.log(this.hero);
-      return;
-    });
+        this.hero = hero;
+        console.log(this.hero);
+        return;
+      });
+  }
+
+  goBack(): void {
+    this.router.navigateByUrl('heroes/list');
   }
 }
--- src/app/modules/heroes/pages/search-page/search-page.component.ts
+++ src/app/modules/heroes/pages/search-page/search-page.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { HeroesService } from '@modules/heroes/services';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html'
 })
 export class SearchPageComponent {
+  public searchInput = new FormControl('')
+
+  private heroesService = inject(HeroesService);
 
 }

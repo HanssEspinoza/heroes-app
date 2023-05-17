6c7b3bedd505027bf6c61e8c4220ef2ce03fd3e5
Hanns Espinoza <hanssabinadie@gmai.com>
2023-05-17 10:01:44 -0600

    feat: :sparkles: add new pages mock

--- src/app/modules/heroes/pages/new-page/new-page.component.ts
+++ src/app/modules/heroes/pages/new-page/new-page.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html'
 })
 export class NewPageComponent {
+  @Input()
+  id?: string;
 
+  public publishers = [
+    {id: 'DC Comics', desc: 'DC - Comics'},
+    {id: 'Marvel Comicas', desc: 'Marvel - Comics'}
+  ];
 }
--- src/app/modules/heroes/pages/search-page/search-page.component.ts
+++ src/app/modules/heroes/pages/search-page/search-page.component.ts
@@ -1,15 +1,37 @@
 import { Component, inject } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 import { HeroesService } from '@modules/heroes/services';
+import { Hero } from '@modules/heroes/models';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html'
 })
 export class SearchPageComponent {
-  public searchInput = new FormControl('')
+  public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+  public selectedHero?: Hero;
 
   private heroesService = inject(HeroesService);
+
+  searchHero(): void {
+    const value: string = this.searchInput.value || '';
+
+    this.heroesService.getSuggestions(value).subscribe((heroes) => this.heroes = heroes);
+  }
+
+  onSelectedOption(event: MatAutocompleteSelectedEvent): void{
+    if ( !event.option.value ) {
+      this.selectedHero = undefined;
+      return
+    }
+
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue(hero.superhero);
+
+    this.selectedHero = hero;
+  }
 
 }

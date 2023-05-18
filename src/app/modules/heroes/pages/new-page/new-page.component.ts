bf2267faf439f90be892edbc12584f82e4ee0bc7
Hanns Espinoza <hanssabinadie@gmai.com>
2023-05-18 16:06:44 -0600

    feat: :sparkles: add reactive form

--- src/app/modules/heroes/pages/new-page/new-page.component.ts
+++ src/app/modules/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '@modules/heroes/models';
 
 @Component({
   selector: 'app-new-page',
@@ -8,8 +10,25 @@ export class NewPageComponent {
   @Input()
   id?: string;
 
+  public heroForm = new FormGroup({
+    id:               new FormControl<string>(''),
+    superhero:        new FormControl<string>('', { nonNullable: true }),
+    publisher:        new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego:        new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters:       new FormControl(''),
+    alt_img:          new FormControl(''),
+  });
+
   public publishers = [
     {id: 'DC Comics', desc: 'DC - Comics'},
     {id: 'Marvel Comicas', desc: 'Marvel - Comics'}
   ];
+
+  onSubmit(): void {
+    console.log({
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value
+    })
+  }
 }
